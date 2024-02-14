5ea525165e6f31b70670a612a55ba5cc6ce00a2e
arif123 <nashrullah2344@gmail.com>
2024-02-14 22:22:15 +0700

    fix no update and delete product

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -5,8 +5,8 @@ const { ResponseError } = require('../errors');
 const productController = {
     getAllProduct: async (req, res) => {
         try {
-            await Product.findAll()
-            sendResponse({res, statusCode:200})
+            const data = await Product.findAll()
+            sendResponse({res, statusCode:200, data:data})
         } catch (error) {
             sendResponse({res, error})
 
@@ -16,7 +16,7 @@ const productController = {
         try {
             const data = await Product.findByPk(req.params.id)
             if (!data) throw new ResponseError('product not found', 404)
-            sendResponse({res, statusCode:200})
+            sendResponse({res, statusCode:200, data: data})
         } catch (error) {
             sendResponse({res, error})
 
@@ -25,24 +25,24 @@ const productController = {
     createNewProduct: async (req, res) => {
         try {
             const data = { ...req.body }
-            await Product.create(data)
-            sendResponse({res, statusCode:200})
+            const newProduct = await Product.create(data)
+            sendResponse({res, statusCode:200,data:newProduct})
         } catch (error) {
             sendResponse({res, error})
         }
     },
-    updateProduct: async () => {
+    updateProduct: async (req, res) => {
         try {
-            const [data] = await Product.update(req.body, { id: req.params.id })
+            const [data] = await Product.update(req.body, { where: { id: req.params.id }})
             if (data === 0) throw ResponseError('product not found', 404)
-            sendResponse({res,statusCode:200})
+            sendResponse({res, statusCode:200,})
         } catch (error) {
             sendResponse({res, error})
         }
     },
     deletedProduct: async (req, res) => {
         try {
-            const data = await Product.destroy(req.params.id)
+            const data = await Product.destroy({ where: { id: req.params.id } })
             if (!data) throw new ResponseError('product not found', 404)
             sendResponse({res, statusCode:200})
         } catch (error) {
